c1c45234615915fcad70991d00fd84ada77b505c
unknown <supenporn.mh@okontek.com>
2026-06-02 11:25:19 +0700

    Add Vercel config and env-based API URL

--- upos/apps/portal/src/api/axios.ts
+++ upos/apps/portal/src/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:4000' })
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:4000',
+})
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('upos_token')
